375e60b0e3217a4fb8bbfcfcf520b3248565058c
minht <minhthuc229@gmail.com>
2017-03-03 08:49:34 +0700

    2st commit

--- src/main/webapp/WEB-INF/static/js/BackOffice.js
+++ src/main/webapp/WEB-INF/static/js/BackOffice.js
@@ -9,3 +9,9 @@ $(".cancel").click(function(){
 /**
  * Created by minht on 2/7/2017.
  */
+function CenterBlock(div1, div2){
+    var w1 = $("#"+div1+"").width();
+    var w2 = $("."+div2+"").width();
+    var margin = (w1-w2)/2;
+    $("."+div2+"").css("margin-left",margin);
+}
